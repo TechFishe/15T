02e4c1bbac653f3e9b4fd624d7d6ca53bf8a48be
TechFishe <jacobb2008@icloud.com>
2023-02-26 14:13:42 -0600

    Cleaned up music page, started about

--- src/routes/About/+page.svelte
+++ src/routes/About/+page.svelte
@@ -1,3 +1,11 @@
+<script>
+    import Main from "$box/main.svelte";
+</script>
+
 <svelte:head>
     <title>About</title>
 </svelte:head>
+
+<Main>
+    <h1>About</h1>
+</Main>
--- src/routes/Music/+page.svelte
+++ src/routes/Music/+page.svelte
@@ -16,7 +16,7 @@
         <h1>Music</h1>
         <p>All of our band&rsquo;s music. All in one place</p>
     </header>
-    <article class="grid justify-center pb-4">
+    <!-- <article class="grid justify-center pb-4">
         <h2 class="border-t-[0.5px] border-light-purple/25 pt-2">Top Songs</h2>
         <ol class="w-fit space-y-6 mt-4">
             <TopSong title="Island Time">
@@ -29,19 +29,19 @@
                 <img src="{EarthImg}" alt="Planet 15T Album Cover" class="w-8 h-[35px]" />
             </TopSong>
         </ol>
-    </article>
+    </article> -->
     <div class="grid grid-col-1 grid-rows-2 gap-y-8">
-        <article class="mainSection"> 
+        <article> 
             <h2>Albums</h2>
-            <section class="grid gird-rows-1 grid-flow-col gap-x-4 justify-start">
+            <section>
                 <AlbumBtn URL="/Music/P15T" title="Planet 15T">
                     <img src="{EarthImg}" alt="Planet 15T Album Cover" class="h-44 w-40" />
                 </AlbumBtn>
             </section>
         </article>
-        <article class="mainSection">
+        <article>
             <h2>Singles &amp; EP&rsquo;s</h2>
-            <section class="grid gird-rows-1 grid-flow-col gap-x-4 justify-start">
+            <section>
                 <AlbumBtn URL="/Music/HPt2" title="Hurricane">
                     <img src="{HurImg}" alt="Hurricane Pic" class="h-44 w-40" />
                 </AlbumBtn>
@@ -51,5 +51,6 @@
 </Main>
 
 <style>
-    .mainSection{ @apply float-left border-t-[0.5px] border-light-purple/25 text-left pt-2 pl-2; }
+    article{ @apply float-left border-t-[0.5px] border-light-purple/25 text-left pt-2 pl-2; }
+    section{ @apply grid grid-rows-1 grid-flow-col gap-x-4 justify-start pt-4; }
 </style>
